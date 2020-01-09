80a733025e0eaaf752cc9cb9e911e8a2b6865e5d
Klaven Jones <klavensjones@gmail.com>
2020-01-08 21:22:13 -0500

    Added Server

---
+++ server/passport.js
@@ -0,0 +1,293 @@
+const passport = require("passport");
+
+//Strategies
+const JWTStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+const LocalStrategy = require("passport-local").Strategy;
+const GoogleTokenStrategy = require("passport-google-token").Strategy;
+const FaceBookTokenStrategy = require("passport-facebook-token");
+const LinkedInTokenStrategy = require("passport-linkedin-token-v2");
+const LinkedInTokenStrategySignUp = require("passport-linkedin-token-v2");
+
+//Config
+const config = require("./config");
+//User Model
+const User = require("./models/userModel");
+
+const cookieExtractor = req => {
+  let token = null;
+  if (req && req.cookies) {
+    token = req.cookies["access_token"];
+  }
+  return token;
+};
+
+//JWT Strategy
+passport.use(
+  new JWTStrategy(
+    {
+      jwtFromRequest: cookieExtractor,
+      secretOrKey: config.JWT_SECRET
+    },
+    async (payload, done) => {
+      try {
+        //Find the user specified in token
+        const user = await User.findById(payload.sub);
+        //If the user does not exist
+        if (!user) {
+          return done(null, false);
+        }
+        return done(null, user);
+      } catch (error) {
+        done(error, false);
+      }
+    }
+  )
+);
+
+//GOOGLE OAUTH STRATEGY
+passport.use(
+  "google-token",
+  new GoogleTokenStrategy(
+    {
+      clientID: config.google.clientID,
+      clientSecret: config.google.clientSecret
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        //Check if user exists in our database
+        let existingUser = await User.findOne({ "google.id": profile.id });
+        if (existingUser) {
+          return done(null, existingUser);
+        }
+
+        // Check if we have someone with the same email
+        existingUser = await User.findOne({
+          "local.email": profile.emails[0].value
+        });
+
+        if (existingUser) {
+          // We want to merge provider's data with local auth
+          existingUser.methods.push("google");
+          existingUser.google = {
+            id: profile.id,
+            email: profile.emails[0].value
+          };
+          await existingUser.save();
+          return done(null, existingUser);
+        }
+
+        //Create New Google User
+        const newUser = new User({
+          methods: ["google"],
+          google: {
+            id: profile.id,
+            email: profile.emails[0].value
+          }
+        });
+        //Saving user
+        console.log("NEW USER");
+        await newUser.save();
+        done(null, newUser);
+      } catch (error) {
+        done(error, false, error.message);
+      }
+    }
+  )
+);
+//FACEBOOK OAUTH STRATEGY
+passport.use(
+  "facebook-token",
+  new FaceBookTokenStrategy(
+    {
+      clientID: config.facebook.clientID,
+      clientSecret: config.facebook.clientSecret
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      console.log("Access Token", accessToken);
+      console.log("Refresh Token", refreshToken);
+      console.log("profile", profile);
+      try {
+        //Check if user exists in our database
+        let existingUser = await User.findOne({ "facebook.id": profile.id });
+        if (existingUser) {
+          console.log("Get existing user");
+          return done(null, existingUser);
+        }
+
+        // Check if we have someone with the same email
+        existingUser = await User.findOne({
+          "local.email": profile.emails[0].value
+        });
+
+        if (existingUser) {
+          console.log("MERGING USER");
+          // We want to merge provider's data with local auth
+          existingUser.methods.push("facebook");
+          existingUser.facebook = {
+            id: profile.id,
+            email: profile.emails[0].value
+          };
+          await existingUser.save();
+          return done(null, existingUser);
+        }
+
+        //Create New Facebook User
+        const newUser = new User({
+          methods: ["facebook"],
+          facebook: {
+            id: profile.id,
+            email: profile.emails[0].value
+          }
+        });
+        //Saving user
+        console.log("NEW USER");
+        await newUser.save();
+        done(null, newUser);
+      } catch (error) {
+        done(error, false, error.message);
+      }
+    }
+  )
+);
+
+//Linkedin OAUTH SIGN IN STRATEGY
+passport.use(
+  "linkedin-token-signin",
+  new LinkedInTokenStrategy(
+    {
+      clientID: config.linkedin.clientID,
+      clientSecret: config.linkedin.clientSecret,
+      redirectURL: config.linkedin.SignInRedirectURL,
+      scope: ["r_emailaddress", "r_liteprofile"]
+    },
+    async (req, accessToken, refreshToken, profile, done) => {
+      console.log("Access Token", accessToken);
+      console.log("Refresh Token", refreshToken);
+      console.log("profile", profile);
+
+      try {
+        //Check if user exists in our database
+        let existingUser = await User.findOne({ "linkedIn.id": profile.id });
+        if (existingUser) {
+          return done(null, existingUser);
+        }
+        // Check if we have someone with the same email
+        existingUser = await User.findOne({
+          "local.email": profile.emails[0].value
+        });
+
+        if (existingUser) {
+          console.log("MERGING USER");
+          // We want to merge provider's data with local auth
+          existingUser.methods.push("linkedIn");
+          existingUser.linkedIn = {
+            id: profile.id,
+            email: profile.emails[0].value
+          };
+          await existingUser.save();
+          return done(null, existingUser);
+        }
+        //Create New Google User
+        const newUser = new User({
+          methods: ["linkedIn"],
+          linkedIn: {
+            id: profile.id,
+            email: profile.emails[0].value
+          }
+        });
+        //Saving user
+        console.log("NEW USER");
+        await newUser.save();
+        done(null, newUser);
+      } catch (error) {
+        done(error, false, error.message);
+      }
+    }
+  )
+);
+
+//Linkedin OAUTH SIGN UP STRATEGY
+passport.use(
+  "linkedin-token-signup",
+  new LinkedInTokenStrategySignUp(
+    {
+      clientID: config.linkedin.clientID,
+      clientSecret: config.linkedin.clientSecret,
+      redirectURL: config.linkedin.SignUpRedirectURL,
+      scope: ["r_emailaddress", "r_liteprofile"]
+    },
+    async (req, accessToken, refreshToken, profile, done) => {
+      console.log("Access Token", accessToken);
+      console.log("Refresh Token", refreshToken);
+      console.log("profile", profile);
+
+      try {
+        //Check if user exists in our database
+        let existingUser = await User.findOne({ "linkedIn.id": profile.id });
+        if (existingUser) {
+          return done(null, existingUser);
+        }
+        // Check if we have someone with the same email
+        existingUser = await User.findOne({
+          "local.email": profile.emails[0].value
+        });
+
+        if (existingUser) {
+          console.log("MERGING USER");
+          // We want to merge provider's data with local auth
+          existingUser.methods.push("linkedIn");
+          existingUser.linkedIn = {
+            id: profile.id,
+            email: profile.emails[0].value
+          };
+          await existingUser.save();
+          return done(null, existingUser);
+        }
+        //Create New Google User
+        const newUser = new User({
+          methods: ["linkedIn"],
+          linkedIn: {
+            id: profile.id,
+            email: profile.emails[0].value
+          }
+        });
+        //Saving user
+        console.log("NEW USER");
+        await newUser.save();
+        done(null, newUser);
+      } catch (error) {
+        done(error, false, error.message);
+      }
+    }
+  )
+);
+
+//Local Strategy
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "email"
+    },
+    async (email, password, done) => {
+      try {
+        //Find user with email
+        const user = await User.findOne({ "local.email": email });
+        //if not handle it
+        if (!user) {
+          return done(null, false);
+        }
+        //If user is found check if password is correct
+        const isMatch = await user.isValidPassword(password);
+        //if not handle it
+        if (!isMatch) {
+          console.log(isMatch);
+          return done(null, false);
+        }
+        done(null, user);
+      } catch (error) {
+        done(null, error);
+      }
+    }
+  )
+);
---
+++ server/routes/userRoutes.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const router = require("express-promise-router")();
+//Controllers
+const userController = require("../controllers/userController.js");
+// Helpers
+const { validateBody, schemas } = require("../helpers/validation");
+//Passport
+const passport = require("passport");
+
+const PassportSignIn = passport.authenticate("local", { session: false });
+
+const PassportJWT = passport.authenticate("jwt", { session: false });
+
+const PassportGoogleToken = passport.authenticate("google-token", {
+  session: false
+});
+
+const PassportFacebookToken = passport.authenticate("facebook-token", {
+  session: false
+});
+
+const PassportLinkedInToken = passport.authenticate("linkedin-token-signin", {
+  session: false
+});
+
+const PassportLinkedInTokenSignUp = passport.authenticate(
+  "linkedin-token-signup",
+  {
+    session: false
+  }
+);
+const PassportConfiguration = require("../passport");
+
+router.get("/dashboard", PassportJWT, userController.dashboard);
+router.get("/sign-out", PassportJWT, userController.signOut);
+router.get("/status", PassportJWT, userController.checkAuth);
+
+router.post(
+  "/sign-up",
+  validateBody(schemas.authSchema),
+  userController.signUp
+);
+
+router.post(
+  "/sign-in",
+  validateBody(schemas.authSchema),
+  PassportSignIn,
+  userController.signIn
+);
+
+router.post("/oauth/google", PassportGoogleToken, userController.googleOAuth);
+
+router.post(
+  "/oauth/facebook",
+  PassportFacebookToken,
+  userController.facebookOAuth
+);
+
+router.post(
+  "/oauth/linkedin/signin",
+  PassportLinkedInToken,
+  userController.linkedInOAuth
+);
+
+router.post(
+  "/oauth/linkedin/signup",
+  PassportLinkedInTokenSignUp,
+  userController.linkedInOAuth
+);
+
+// router.post(
+//   "/oauth/linkedin",
+//   PassportLinkedInToken,
+//   userController.linkedinOAuth
+// );
+
+module.exports = router;
